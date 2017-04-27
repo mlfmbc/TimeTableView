afd69d20df92c2fbf1193ee5a73501a919c9a857
mlfmbc <983442042@qq.com>
2017-04-27 13:37:05 +0800

    保存到本地

--- app/src/main/java/com/shallcheek/timetale/ScreenshotUtil.java
+++ app/src/main/java/com/shallcheek/timetale/ScreenshotUtil.java
@@ -1,11 +1,16 @@
 package com.shallcheek.timetale;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.ScrollView;
 import android.widget.Toast;
@@ -21,8 +26,9 @@ import java.io.IOException;
  */
 public class ScreenshotUtil {
 
-    private final static String FILE_SAVEPATH = Environment
-            .getExternalStorageDirectory().getAbsolutePath() ;
+    private final static String FILE_SAVEPATH =
+            Environment
+            .getExternalStorageDirectory().getAbsolutePath()+"" ;
     public static String pathfile = FILE_SAVEPATH + "/ScreenshotUtil.png";
     public static int h = 0;
 
@@ -30,7 +36,7 @@ public class ScreenshotUtil {
      * 因为课表是可以滑动 的所以截取
      * 截取scrollview的屏幕
      **/
-    public static void getBitmapByView(Context mContext, final ScrollView scrollView) {
+    public static void getBitmapByView(final Context mContext, final ScrollView scrollView) {
         // 获取listView实际高度
         h = 0;
         for (int i = 0; i < scrollView.getChildCount(); i++) {
@@ -42,35 +48,101 @@ public class ScreenshotUtil {
         Canvas canvas = new Canvas(bitmap);
         scrollView.draw(canvas);
 
-        Bitmap head = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.share_term_table_header);
-        Bitmap foot = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.share_term_table_footer);
-        Bitmap v = toConformBitmap(head, bitmap, foot);
 
-        File savedir = new File(FILE_SAVEPATH);
-        if (!savedir.exists()) {
-            savedir.mkdirs();
+
+
+        Bitmap newbmp = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas cv = new Canvas(newbmp);
+        cv.drawBitmap(bitmap, 0, 0, null);
+        cv.save(Canvas.ALL_SAVE_FLAG);// 保存
+        cv.restore();// 存储
+        //回收
+        bitmap.recycle();
+
+
+
+
+
+//        Bitmap head = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.share_term_table_header);
+//        Bitmap foot = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.share_term_table_footer);
+//        Bitmap v = toConformBitmap(head, bitmap, foot);
+
+        File appDir = new File(Environment.getExternalStorageDirectory(), "迈迈Mec");//迈迈
+        if (!appDir.exists()) {
+            appDir.mkdir();
         }
+
+//        File savedir = new File(FILE_SAVEPATH);
+//        if (!savedir.exists()) {
+//            savedir.mkdirs();
+//        }
+//        pathfile=FILE_SAVEPATH+"/"+System.currentTimeMillis() + ".png";
+        String fileName = System.currentTimeMillis() + ".png";
+        File file = new File(appDir, fileName);
 
         FileOutputStream out = null;
         try {
-            out = new FileOutputStream(pathfile);
+            out = new FileOutputStream(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             Toast.makeText(mContext,"保存失败",Toast.LENGTH_SHORT).show();
         }
         try {
             if (null != out) {
-                v.compress(Bitmap.CompressFormat.PNG, 100, out);
+                newbmp.compress(Bitmap.CompressFormat.PNG, 100, out);
                 out.flush();
                 out.close();
             }
-            Toast.makeText(mContext,"保存成功",Toast.LENGTH_SHORT).show();
+            String mUri="";
+            // 其次把文件插入到系统图库
+//            try {
+//                 mUri=MediaStore.Images.Media.insertImage(mContext.getContentResolver(),
+//                        file.getAbsolutePath(), fileName, null);
+//                Log.e("111>>>>>>",""+file.getAbsolutePath()+"____"+fileName+"______"+mUri);
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            }
+
+//            if(hasKitkat()){
+//                MediaScannerConnection.scanFile(mContext,
+//                        new String[] { appDir.getAbsolutePath() }, new String[]{ "mmm/*" },
+//                        new MediaScannerConnection.OnScanCompletedListener() {
+//                            public void onScanCompleted(String path, Uri uri) {
+//                                mContext.sendBroadcast(new Intent(android.hardware.Camera.ACTION_NEW_PICTURE, uri));
+//                                mContext.sendBroadcast(new Intent("com.android.camera.NEW_PICTURE", uri));
+//                            }
+//                        });
+//                scanPhotos(file.getAbsolutePath(), mContext); // 实际起作用的方法
+//            }else{
+//                mContext.sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://"+ Environment.getExternalStorageDirectory())));
+//            }
+
+String f="file:" +file.getAbsolutePath();
+            // 最后通知图库更新
+            mContext.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse(f)));
+//            mContext.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(new File(file.getPath()))));
+
+            //发广播告诉相册有图片需要更新，这样可以在图册下看到保存的图片了
+//            Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+//            Uri uri = Uri.fromFile(file);
+//            intent.setData(uri);
+//            mContext.sendBroadcast(intent);
+            Log.e(">>>>>>",""+file.getAbsolutePath()+"____"+file.getName()+"---"+f);
+            Toast.makeText(mContext,"保存成功___"+mUri,Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
             Toast.makeText(mContext,"保存失败",Toast.LENGTH_SHORT).show();
         }
 
     }
-
+    public static void scanPhotos(String filePath, Context context) {
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri uri = Uri.fromFile(new File(filePath));
+        intent.setData(uri);
+        context.sendBroadcast(intent);
+    }
+    public static boolean hasKitkat() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+    }
     /**
      * 合并图片
      *
